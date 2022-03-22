8743333eb18792550b9b9a52080eb66b7b3fd083
themysteriousduong <nguyentuanduong097@gmail.com>
2022-03-22 21:33:23 +0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
-// const console = require("console");
-
-// $( document ).ready(function() {
-//     $(".body-container").fadeIn(1000);
-//     console.log("READY");
-// });
-
 $(document).ready(function () {
     console.log("READY");
-    var body = $('.body-container')
-    body.hide().slideDown({duration:1000, queue:false});
-    body.hide().fadeIn({duration:1000, queue:false});
+    const body = $('.body-container');
+    console.log('body', body);
+    body.addClass('show');
 });
